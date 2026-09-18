ef2a9b7808e9533576a78de2a4ce970870ec06db
JINMMYSC <oneyouhome@live.com>
2026-09-18 13:53:04 +0800

    feat: centralize stage3 message policies

--- src/WXDataStudio.App/Models/MessageKind.cs
+++ src/WXDataStudio.App/Models/MessageKind.cs
@@ -25,4 +25,11 @@ public static class MessageKindPolicy
 {
     public static bool IsSensitive(MessageKind kind) => kind is
         MessageKind.Transfer or MessageKind.RedPacket or MessageKind.Payment;
+
+    public static bool IsEditable(MessageKind kind) => !IsSensitive(kind) && kind is not
+        MessageKind.Unknown and not MessageKind.System;
+
+    public static bool HasExternalMedia(MessageKind kind) => kind is
+        MessageKind.Image or MessageKind.Voice or MessageKind.Video or
+        MessageKind.Emoji or MessageKind.File;
 }
